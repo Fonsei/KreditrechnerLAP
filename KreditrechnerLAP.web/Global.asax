ec39762b0bc3612a19595efb15ce1a574eeff72e
XYZ <XYZ@DESKTOP-JLMI9RA>
2016-11-08 09:51:47 +0100

    Add project files.

---
+++ KreditrechnerLAP.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KreditrechnerLAP.web.MvcApplication" Language="C#" %>
---
+++ KreditrechnerLAP.web/Models/KreditRechnerModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KreditrechnerLAP.web.Models
+{
+    public class KreditRechnerModel
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [Range(3000,40000)]
+        [DefaultValue(3000)]
+        public int Kreditbetrag { get; set; }
+
+        [Required]
+        [Range(12,60)]
+        [DefaultValue(12)]
+        public int Laufzeit { get; set; }
+
+        [Required]
+        public double RateMonatlich { get; set; }
+
+
+    }
+}
