55c7e767f778835a3ae6ce332e5e3d5196ca461b
XYZ <XYZ@DESKTOP-JLMI9RA>
2016-11-09 09:45:25 +0100

    Controller erweitert

--- KreditrechnerLAP.web/Controllers/KreditRechnerController.cs
+++ KreditrechnerLAP.web/Controllers/KreditRechnerController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using KreditrechnerLAP.web.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,50 @@ namespace KreditrechnerLAP.web.Controllers
         [HttpGet]
         public ActionResult KreditRechner()
         {
+            Debug.WriteLine("GET - KreditRechnerController - KreditRechner");
+            Debug.Indent();
+
+
+            Debug.Unindent();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult KreditRechner(KreditRechnerModel model)
+        {
+            Debug.WriteLine("POST - KreditRechnerController - KreditRechner");
+            Debug.Indent();
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Finanzielles");
+            }
+            Debugger.Break();
+            Debug.Unindent();
+            return View(model);
+        }
+
+        public ActionResult Finanzielles()
+        {
+            Debug.WriteLine("GET - KreditRechnerController - Finanzielles");
+            Debug.Indent();
+
+
+            Debug.Unindent();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Finanzielles(KreditRechnerModel model)
+        {
+            Debug.WriteLine("POST - KreditRechnerController - Finanzielles");
+            Debug.Indent();
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Finanzielles");
+            }
+
+            Debug.Unindent();
+            return View(model);
         }
     }
 }
