1c3c03d6e175f5228355e652e08b5cdddea40bbe
XYZ <XYZ@DESKTOP-JLMI9RA>
2016-11-10 09:22:08 +0100

    Kredit Rechner Design, Finanzielles Model erstellt und Viwe erstellt

--- KreditrechnerLAP.web/Models/FinanziellesModel.cs
+++ KreditrechnerLAP.web/Models/FinanziellesModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,23 @@ namespace KreditrechnerLAP.web.Models
     public class FinanziellesModel
     {
 
-        public int ID { get; set; }
-
+        [Required(ErrorMessage = "Pflichtfeld")]
+        [Display(Name = "Monatliches Netto Einkommen")]
         public double NettoEinkommen { get; set; }
+
+        [Required(ErrorMessage = "Pflichtfeld")]
+        [Display(Name = "Wohnkosten(Miete,Strom,Heizung)")]
+        public double Wohnkosten { get; set; }
+
+        [Display(Name = "Aliemente die Sie bekommen")]
+        public double EinkünfteAlimente { get; set; }
+
+        [Display(Name = "Unterhalt den sie Erhalten")]
+        public double Unterhalt { get; set; }
+
+
+        [Display(Name = "Monatliche raten(Kredit)")]
+        public double RatenVerpflichtung { get; set; }
 
 
     }
